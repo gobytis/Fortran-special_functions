a5aac9c119ecd7a15f7a01273a768b3bd669b6a9
yokota.kohei <kohei.desu105@gmail.com>
2023-09-17 16:56:57 +0900

    fpm.toml から不要なモジュールを削除しました。special_functions.f90 にコメントを追加しました。

--- src/special_functions.f90
+++ src/special_functions.f90
@@ -14,22 +14,32 @@ module special_functions
     public :: digamma, trigamma, polygamma
 
     !> ベータ関数です。
+    !> `beta(a, b)` = $int_0^1 t^{a-1} (1 - t)^{a - 1} dt$
     interface beta
         pure module function int_beta(a, b) result(retval)
+            !> argument 1
             integer, intent(in) :: a
+            !> argument 2
             integer, intent(in) :: b
+            !> 戻り値
             real(dp) :: retval
         end function
 
         pure module function sp_beta(a, b) result(retval)
+            !> argument 1
             real(sp), intent(in) :: a
+            !> argument 2
             real(sp), intent(in) :: b
+            !> 戻り値
             real(sp) :: retval
         end function
 
         pure module function dp_beta(a, b) result(retval)
+            !> argument 1
             real(dp), intent(in) :: a
+            !> argument 2
             real(dp), intent(in) :: b
+            !> 戻り値
             real(dp) :: retval
         end function
     end interface
@@ -38,7 +48,9 @@ module special_functions
     !> ディガンマ関数用のラッパーです。
     interface digamma
         impure module function dp_digamma(x) result(retval)
+            !> 入力
             real(dp), intent(in) :: x
+            !> 戻り値
             real(dp) :: retval
         end function
     end interface
@@ -47,7 +59,9 @@ module special_functions
     !> トリガンマ関数用のラッパーです。
     interface trigamma
         impure module function dp_trigamma(x) result(retval)
+            !> 入力
             real(dp), intent(in) :: x
+            !> 戻り値
             real(dp) :: retval
         end function
     end interface
@@ -56,8 +70,12 @@ module special_functions
     !> ポリガンマ関数です。
     interface polygamma
         impure recursive module function dp_polygamma(k, x) result(retval)
+            !> 関数パラメータ
+            !> k = 0, 1, 2, corresponds to digamma, trigamma, tetragamma.... respectively.
             integer, intent(in) :: k
+            !> 入力
             real(dp), intent(in) :: x
+            !> 戻り値
             real(dp) :: retval
         end function
     end interface
